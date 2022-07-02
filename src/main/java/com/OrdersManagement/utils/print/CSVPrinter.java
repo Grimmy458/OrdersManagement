909d992da9e979e165e9b2775b94e07a1b80722d
Grimmy458 <108542969+Grimmy458@users.noreply.github.com>
2022-07-02 03:00:11 +0300

    Fixed orders export with cyrillic symbols.

--- src/main/java/com/OrdersManagement/utils/print/CSVPrinter.java
+++ src/main/java/com/OrdersManagement/utils/print/CSVPrinter.java
@@ -1,18 +1,17 @@
 package com.OrdersManagement.utils.print;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class CSVPrinter {
 
     public static void print(String filePath, String[] headers, ArrayList<ArrayList<String>> data) throws IOException {
-        FileWriter fileWriter = null;
         BufferedWriter bufferedWriter = null;
         try {
-            fileWriter = new FileWriter(filePath);
-            bufferedWriter = new BufferedWriter(fileWriter);
+            File file = new File(filePath);
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter
+                    (new FileOutputStream(file), StandardCharsets.UTF_8));
             bufferedWriter.write(String.join(",", headers));
             bufferedWriter.newLine();
             for (ArrayList<String> row : data) {
@@ -23,14 +22,6 @@ public class CSVPrinter {
             try {
                 if (bufferedWriter != null) {
                     bufferedWriter.close();
-                }
-            } catch (java.io.IOException ex) {
-                ex.printStackTrace();
-            }
-
-            try {
-                if (fileWriter != null) {
-                    fileWriter.close();
                 }
             } catch (java.io.IOException ex) {
                 ex.printStackTrace();
